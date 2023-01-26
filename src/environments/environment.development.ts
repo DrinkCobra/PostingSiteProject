b7ebd5183b66687f24c86bbece7ecab1066233f3
DrinkCobra <aldrinmacatangay0301@gmail.com>
2023-01-26 19:26:29 +0800

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyDJJBCGpth_9gKvRbDedtYO1XKnLj92l_Q",
+        authDomain: "postingsiteproject.firebaseapp.com",
+        projectId: "postingsiteproject",
+        storageBucket: "postingsiteproject.appspot.com",
+        messagingSenderId: "1080552694500",
+        appId: "1:1080552694500:web:01eaef76e0e3630e3e42f1",
+        measurementId: "G-B9V389Y6SX"
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    firebaseConfig: {
+        apiKey: "AIzaSyDJJBCGpth_9gKvRbDedtYO1XKnLj92l_Q",
+        authDomain: "postingsiteproject.firebaseapp.com",
+        projectId: "postingsiteproject",
+        storageBucket: "postingsiteproject.appspot.com",
+        messagingSenderId: "1080552694500",
+        appId: "1:1080552694500:web:01eaef76e0e3630e3e42f1",
+        measurementId: "G-B9V389Y6SX"
+    }
+};
